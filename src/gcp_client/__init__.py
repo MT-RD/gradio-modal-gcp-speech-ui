1b08ede3d2a02239a8fecd42c83f7bbb3526bff0
Manoj Talagadadeevi <manojt.854@gmail.com>
2025-06-10 19:06:37 -0400

    Add initial GCP Client module with documentation for Speech-to-Text implementation

---
+++ src/gcp_client/__init__.py
@@ -0,0 +1,8 @@
+"""
+GCP Client Module
+
+This module contains the Google Cloud Platform Speech-to-Text client implementation.
+"""
+
+# Placeholder for GCP STT client implementation
+# Will be implemented in Commit 2: Google Cloud Setup & Configuration
