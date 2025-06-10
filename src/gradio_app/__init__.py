d0b95034136b31bb6e712332e4eec123047641c7
Manoj Talagadadeevi <manojt.854@gmail.com>
2025-06-10 19:06:26 -0400

    Add Gradio app module documentation for speech-to-text interface

---
+++ src/gradio_app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Gradio App Module
+
+This module contains the Gradio frontend application for the speech-to-text interface.
+"""
+
+# Placeholder for Gradio app implementation
+# Will be implemented in Commit 5: Basic Gradio Interface
