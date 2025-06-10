e45bc7c5338ad079436e625a7d00ba7a87477e77
Manoj Talagadadeevi <manojt.854@gmail.com>
2025-06-10 19:06:10 -0400

    Initialize Gradio Modal GCP Speech UI package with versioning and author information

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Gradio Modal GCP Speech UI Package
+
+A modern speech-to-text application using Gradio, Modal, and Google Cloud Speech-to-Text.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Manoj T"
+__email__ = "your.email@example.com"
