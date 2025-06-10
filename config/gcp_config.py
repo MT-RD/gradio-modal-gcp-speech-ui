df87a38e706428b94272533bc91dac1904b6393d
Manoj Talagadadeevi <manojt.854@gmail.com>
2025-06-10 19:33:44 -0400

    Add GCP configuration module (skeleton) for Speech-to-Text API

---
+++ config/gcp_config.py
@@ -0,0 +1,20 @@
+"""
+Google Cloud Platform Configuration
+
+This module contains specific configuration for Google Cloud Speech-to-Text API.
+"""
+
+from typing import Dict, Any, List
+# from config.settings import settings  # TODO: Will be used in detailed implementation
+
+
+class GCPConfig:
+    """Google Cloud Platform configuration for Speech-to-Text API."""
+    
+    # TODO: Add Speech-to-Text API Configuration
+    # TODO: Add Audio format configurations  
+    # TODO: Add Supported language codes
+    # TODO: Add class methods for configuration handling
+    # TODO: Add file size and duration limits
+    
+    pass
