8191061dc57cc86a5a6ef0069019e08fcae9066d
Manoj Talagadadeevi <manojt.854@gmail.com>
2025-06-10 20:38:55 -0400

    Add setup (skeleton) script for Gradio Modal GCP Speech UI development environment

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Setup script for Gradio Modal GCP Speech UI
+# This script sets up the development environment
+
+set -e
+
+echo "🚀 Setting up Gradio Modal GCP Speech UI development environment..."
+
+# TODO: Add Python version check
+# TODO: Add virtual environment setup
+# TODO: Add dependencies installation
+# TODO: Add environment file setup
+# TODO: Add FFmpeg check and completion message
+
+echo "🔧 Setup script skeleton ready for incremental implementation..."
